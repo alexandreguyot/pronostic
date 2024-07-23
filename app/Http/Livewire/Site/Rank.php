22c79ac1a81d2690d61038c9df71b4205d7c60b7
Alexandre Guyot <a.pro.guyot@gmail.com>
2024-07-23 17:09:27 +0200

    Mise a jour

---
+++ app/Http/Livewire/Site/Rank.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Livewire\Site;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
+use App\Models\Sport;
+use App\Models\Pronostic;
+use Illuminate\Support\Collection;
+
+class Rank extends Component
+{
+    public $leagues;
+    public $selectedLeagueId;
+    public $selectedSportTitle;
+    public $leaguesWithPoints;
+    public $sports;
+
+    public function mount()
+    {
+        $this->leagues = Auth::user()->leagues()->with(['users.pronostics.game.sport', 'sports'])->get();
+        $this->selectedLeagueId = $this->leagues->first()->id ?? null;
+        $this->updateSports();
+        $this->selectedSportTitle = $this->sports->first() ?? null;
+        $this->updateLeaguesWithPoints();
+    }
+
+    public function updatedSelectedLeagueId()
+    {
+        $this->updateSports();
+        $this->selectedSportTitle = $this->sports->first() ?? null;
+        $this->updateLeaguesWithPoints();
+    }
+
+    public function updatedSelectedSportTitle()
+    {
+        $this->updateLeaguesWithPoints();
+    }
+
+    private function updateSports()
+    {
+        $selectedLeague = $this->leagues->firstWhere('id', $this->selectedLeagueId);
+        if ($selectedLeague) {
+            $this->sports = $selectedLeague->sports->map(function ($sport) {
+                return in_array($sport->id, [5, 6, 7]) ? 'Médailles' : $sport->title;
+            })->unique()->concat(collect(['Total']));
+        } else {
+            $this->sports = collect();
+        }
+    }
+
+    private function updateLeaguesWithPoints()
+    {
+        $this->leaguesWithPoints = $this->leagues->map(function ($league) {
+            $users = $league->users->map(function ($user) use ($league) {
+                $totalPoints = 0;
+                $sportsPoints = collect();
+
+                foreach ($user->pronostics as $pronostic) {
+                    $game = $pronostic->game;
+                    if ($league->sports->contains($game->sport)) {
+                        $sportTitle = in_array($game->sport->id, [5, 6, 7]) ? 'Médailles' : $game->sport->title;
+                        if (!$sportsPoints->has($sportTitle)) {
+                            $sportsPoints->put($sportTitle, 0);
+                        }
+                        if ($game->date_time < Carbon::now() && $game->home_score !== null && $game->exterior_score !== null) {
+                            $points = $pronostic->points ?? 0;
+                            $totalPoints += $points;
+                            $sportsPoints[$sportTitle] += $points;
+                        }
+                    }
+                }
+
+                // Ajouter les points au modèle utilisateur
+                $user->setAttribute('total_points', $totalPoints);
+                $user->setAttribute('sports_points', $sportsPoints);
+
+                return $user;
+            })->sortByDesc('total_points');
+
+            // Ajouter le groupe "Total" pour les points totaux de tous les sports
+            $usersWithTotal = $users->map(function ($user) {
+                $totalPoints = $user->total_points;
+                $user->sports_points->put('Total', $totalPoints);
+                return $user;
+            });
+
+            return [
+                'league' => $league,
+                'users' => $usersWithTotal,
+            ];
+        });
+    }
+
+    public function render()
+    {
+        return view('livewire.site.rank', [
+            'leaguesWithPoints' => $this->leaguesWithPoints,
+            'leagues' => $this->leagues,
+            'sports' => $this->sports,
+        ]);
+    }
+}
---
+++ resources/views/livewire/site/rank.blade.php
@@ -0,0 +1,54 @@
+<div class="overflow-x-hidden w-full">
+    <div>
+        <nav class="flex w-full justify-around mb-4 overflow-x-auto flex-nowrap max-w-full scrollable-element" aria-label="Tabs">
+            @foreach ($leagues as $league)
+                <a href="#" wire:click.prevent="$set('selectedLeagueId', {{ $league->id }})" class="league-tab whitespace-nowrap py-4 px-1 border-b-2 font-medium text-lg {{ $selectedLeagueId == $league->id ? 'border-focusBlueSite text-focusBlueSite' : '' }}">
+                    {{ $league->title }}
+                </a>
+            @endforeach
+        </nav>
+
+        <!-- Navigation pour les sports -->
+        <nav class="flex w-full justify-around mb-4 overflow-x-auto flex-nowrap max-w-full" aria-label="Tabs">
+            @foreach ($sports as $sport)
+                <a href="#" wire:click.prevent="$set('selectedSportTitle', '{{ $sport }}')" class="sport-tab whitespace-nowrap py-4 px-4 border-b-2 font-medium text-lg {{ $selectedSportTitle == $sport ? 'border-focusBlueSite text-focusBlueSite' : '' }}">
+                    {{ $sport }}
+                </a>
+            @endforeach
+        </nav>
+
+        @foreach ($leaguesWithPoints as $leagueData)
+            @if ($leagueData['league']->id == $selectedLeagueId)
+                <div class="mb-8">
+                    <table class="w-full bg-transparent">
+                        <thead class="w-full">
+                            <tr class="w-full border-b-2 border-t-2 border-b-white/30 border-t-white/30">
+                                <th class="py-2 px-4 text-lg text-center leading-4 font-medium text-focusBlueSite uppercase tracking-wider border-right w-1/5"></th>
+                                <th class="w-3/5 py-2 px-4 text-lg text-center leading-4 font-medium text-focusBlueSite uppercase tracking-wider border-r-2 border-r-white/30">Joueur</th>
+                                <th class="w-1/5 py-2 px-4 text-lg text-center leading-4 font-medium text-focusBlueSite uppercase tracking-wider">Points</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-transparent">
+                            @foreach ($leagueData['users'] as $index => $user)
+                                @if ($user->sports_points->has($selectedSportTitle))
+                                    <tr class="text-center border-b-2 border-b-white/30">
+                                        <td class="py-2 px-4 border-right">{{ $index + 1 }}</td>
+                                        <td class="py-2 px-4 border-right">{{ $user->firstname }} {{ $user->name }}</td>
+                                        <td class="py-2 px-4 text-white bg-focusBlueSite/30 relative group">
+                                            @if($selectedSportTitle === 'Total')
+                                                {{ $user->sports_points['Total'] ?? 0 }}
+                                            @else
+                                                {{ $user->sports_points[$selectedSportTitle] ?? 0 }}
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endif
+        @endforeach
+    </div>
+</div>
+
